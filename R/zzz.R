86dbda89b7b303143e26858f57ea83c7c373b954
nelstevens <nelson.stevens92@gmail.com>
2021-10-16 13:53:09 +0200

    FIXME: initialize python when not installed not working
    install_python(3.6.8) returns error: cannot change working directory

--- R/zzz.R
+++ R/zzz.R
@@ -35,10 +35,27 @@
   #   # FIXME: activate env
   # }
   #make environment
-  reticulate::virtualenv_create(envname = pkgname, python = "3.6.8")
-  virtualenv_install(envname = pkgname, "opencv-python")
-  virtualenv_install(envname = pkgname, "scikit-image")
-  virtualenv_install(envname = pkgname, "tqdm")
-  virtualenv_install(envname = pkgname, "pandas")
-  virtualenv_install(envname = pkgname, "utils")
+  browser()
+  tryCatch(
+    reticulate::virtualenv_create(envname = pkgname, python = "3.6.8"),
+    error = function(e) {
+      boo <- askYesNo(
+        "Python 3.6.8 is not installed on your system. Would you like to install it?",
+        default = F
+      )
+      if (boo) {
+        reticulate::install_python(version = "3.6.8")
+        reticulate::virtualenv_create(envname = pkgname, python = "3.6.8")
+      }
+      else {
+        stop("Please install python3.6.8 before proceeding")
+      }
+      cat(boo)
+    }
+  )
+  # virtualenv_install(envname = pkgname, "opencv-python")
+  # virtualenv_install(envname = pkgname, "scikit-image")
+  # virtualenv_install(envname = pkgname, "tqdm")
+  # virtualenv_install(envname = pkgname, "pandas")
+  # virtualenv_install(envname = pkgname, "utils")
 }
