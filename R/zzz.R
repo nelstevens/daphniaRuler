070e20c3621e36b94fb8ad6de0541fd1dac34b61
nelstevens <nelson.stevens92@gmail.com>
2022-02-07 22:40:20 +0100

    fixed zzz

--- R/zzz.R
+++ R/zzz.R
@@ -1,31 +1,69 @@
 .onLoad <- function(libname, pkgname) {
-  #make environment and install python if necessary
-  tryCatch(
-    {
-      packageStartupMessage("trying to initialize virtual environment")
-      reticulate::virtualenv_create(envname = pkgname, python = "3.6.8")
-    },
-    error = function(e) {
-      boo <- askYesNo(
-        "Python 3.6.8 is not installed on your system. Would you like to install it?",
-        default = F
+  packageStartupMessage("Trying to find python...")
+  # try finding and using installed python
+  pythonpath <- reticulate::py_discover_config()
+  tryCatch({
+    reticulate::use_python(pythonpath$python)
+  },
+  error = function(mgs) {
+    stop("No usable python installation found. please install python that reticulate can acess. Maybe reticualte::install_miniconda() will work.")
+  })
+  packageStartupMessage("Trying to find conda environment 'daphniaRuler'")
+  # check if conda environment exists
+  exi <- reticulate::conda_list()
+  if (!pkgname %in% exi$name) {
+    packageStartupMessage("Creating conda environment 'daphniaRuler'")
+    # create virtual env
+    reticulate::conda_create(envname = pkgname, python_version = "3.6")
+    # install daphniaruler
+    packageStartupMessage("installing daphniaruler python package")
+    reticulate::virtualenv_install(
+      envname = pkgname,
+      packages = c(
+        "astroid==2.3.3",
+        "attrs==21.2.0",
+        "cycler==0.10.0",
+        "decorator==4.4.1",
+        "imageio==2.6.1",
+        "iniconfig==1.1.1",
+        "isort==4.3.21",
+        "kiwisolver==1.1.0",
+        "lazy-object-proxy==1.4.3",
+        "matplotlib==3.1.2",
+        "mccabe==0.6.1",
+        "networkx==2.4",
+        "numpy==1.17.4",
+        "opencv-python==4.1.2.30",
+        "packaging==21.0",
+        "pandas==0.25.3",
+        "Pillow==6.2.1",
+        "pluggy==1.0.0",
+        "py==1.10.0",
+        "pylint==2.4.4",
+        "pyparsing==2.4.5",
+        "pytest==6.2.5",
+        "python-dateutil==2.8.1",
+        "pytz==2019.3",
+        "PyWavelets==1.1.1",
+        "scikit-image==0.16.2",
+        "scipy==1.3.3",
+        "six==1.13.0",
+        "toml==0.10.2",
+        "tqdm==4.40.2",
+        "typed-ast==1.4.0",
+        "wrapt==1.11.2"
       )
-      if (boo) {
-        packageStartupMessage("installing python3.6.8")
-        reticulate::install_python(version = "3.6.8")
-        packageStartupMessage(sprintf("creating virtual environment: %s", pkgname))
-        reticulate::virtualenv_create(envname = pkgname, python = "3.6.8")
-      }
-      else {
-        stop("Please install python3.6.8 before proceeding")
-      }
-      cat(boo)
-    }
-  )
-  packageStartupMessage("installing dependencies to virtual environment")
-  virtualenv_install(envname = pkgname, "opencv-python")
-  virtualenv_install(envname = pkgname, "scikit-image")
-  virtualenv_install(envname = pkgname, "tqdm")
-  virtualenv_install(envname = pkgname, "pandas")
-  virtualenv_install(envname = pkgname, "utils")
+    )
+    reticulate::virtualenv_install(
+      envname = pkgname,
+      packages = "daphruler==0.3.1",
+      pip_options = "-i https://test.pypi.org/simple/"
+    )
+    packageStartupMessage("Activating conda environment 'daphniaRuler'")
+    reticulate::use_condaenv(pkgname)
+  } else {
+    packageStartupMessage("Activating conda environment 'daphniaRuler'")
+    reticulate::use_condaenv(pkgname)
+  }
+  packageStartupMessage("done... have fun")
 }
--- load_ruler.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# delete everything after __name__ == __main__ in measurement methods
-cat ./inst/python/The-Daphnia-ruler/measurement_methods.py | sed '/^if __name__ ==/q' | head -n -1 > ./inst/python/The-Daphnia-ruler/measurement_methods_r.py
-
-# delete everything after __name__ == __main__ in daphnia_ruler
-cat ./inst/python/The-Daphnia-ruler/daphnia_ruler.py | sed '/^if __name__ ==/q' | head -n -1 > ./inst/python/The-Daphnia-ruler/daphnia_ruler_r.py
