b524f56c4e0e9e5684aacab408849b4bbaa4bf74
nelstevens <nelson.stevens92@gmail.com>
2022-02-16 19:05:37 +0100

    test ospath

--- tests/testthat/test-measure_image.R
+++ tests/testthat/test-measure_image.R
@@ -1,3 +1,16 @@
 test_that("pltimage works", {
   vdiffr::expect_doppelganger("pltimg", pltimg(test_data$test_arra))
 })
+test_that("ospath works", {
+  if (Sys.info()[["sysname"]] == "Windows") {
+    expect_equal(
+      ospath("C:/Users/Nelson/Desktop/ruler/dphrl"),
+      "C:\\Users\\Nelson\\Desktop\\ruler\\dphrl"
+      )
+  } else {
+    expect_equal(
+      ospath("/c/Users/Nelson/Desktop/ruler/dphrl"),
+      "/c/Users/Nelson/Desktop/ruler/dphrl"
+    )
+  }
+})
