df9140221027d849a3139f1201ea7e6c902085b0
nelstevens <nelson.stevens92@gmail.com>
2022-02-22 23:08:32 +0100

    added test for wrong path

--- tests/testthat/test-measure_directory.R
+++ tests/testthat/test-measure_directory.R
@@ -2,4 +2,5 @@ test_that("measure_directory works", {
   expect_error(measure_directory(1.5))
   expect_error(measure_directory(10L))
   expect_error(measure_directory(TRUE))
+  expect_error(measure_directory("wrong/directory"))
 })
