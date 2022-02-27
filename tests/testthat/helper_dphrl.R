53f0a1604d2ee5d06ef1401a1155599fec396e9e
nelstevens <nelson.stevens92@gmail.com>
2022-02-27 22:41:43 +0100

    add simple tests

--- tests/testthat/helper_dphrl.R
+++ tests/testthat/helper_dphrl.R
@@ -1,2 +1,4 @@
 corrpath <- system.file("sample_images/example1.JPG", package = "daphniaruler")
 corrpath_head <- system.file("sample_images/example_head.JPG", package = "daphniaruler")
+
+corrpath_dir <- system.file("sample_images", package = "daphniaruler")
--- tests/testthat/test-measure_directory.R
+++ tests/testthat/test-measure_directory.R
@@ -3,4 +3,6 @@ test_that("measure_directory works", {
   expect_error(measure_directory(10L))
   expect_error(measure_directory(TRUE))
   expect_error(measure_directory("wrong/directory"))
+  expect_error(measure_directory(corrpath_dir, eye_method = "one"))
+  expect_error(measure_directory(corrpath_dir, write_images = "one"))
 })
