a4a5dcde610ce6a75ea8e1a070214a0a8f7c1b8e
nelstevens <nelson.stevens92@gmail.com>
2023-04-01 18:19:16 +0200

    omit non relevant tests

--- tests/testthat/test-measure_directory.R
+++ tests/testthat/test-measure_directory.R
@@ -6,23 +6,4 @@ test_that("measure_directory works", {
   expect_error(measure_directory(corrpath_dir, eye_method = "one"))
   expect_error(measure_directory(corrpath_dir, write_images = "one"))
   expect_error(measure_directory(corrpath_dir, scaling_factor = "bogus"))
-  # expect warning with eye method
-  expect_warning(measure_directory(corrpath_dir, write_images = F))
-  # outputs a list
-  expect_type(suppressWarnings(measure_directory(corrpath_dir, write_images = F)), "list")
-  expect_type(measure_directory(corrpath_dir, eye_method = FALSE, write_images = F), "list")
-  # snapshot test
-  expect_snapshot(measure_directory(corrpath_dir, write_images = F, eye_method = F), variant = Sys.info()[["sysname"]])
-  expect_snapshot(suppressWarnings(measure_directory(corrpath_dir, write_images = F, eye_method = T)), variant = Sys.info()[["sysname"]])
-  expect_snapshot(measure_directory(corrpath_dir, write_images = T, eye_method = F), variant = Sys.info()[["sysname"]])
-  expect_snapshot(suppressWarnings(measure_directory(corrpath_dir, write_images = T, eye_method = T)), variant = Sys.info()[["sysname"]])
-  expect_snapshot(measure_directory(corrpath_dir, write_images = F, eye_method = F, scaling_factor = 102), variant = Sys.info()[["sysname"]])
-  expect_snapshot(suppressWarnings(measure_directory(corrpath_dir, write_images = F, eye_method = T, scaling_factor = 102)), variant = Sys.info()[["sysname"]])
-  expect_snapshot(measure_directory(corrpath_dir, write_images = T, eye_method = F, scaling_factor = 102), variant = Sys.info()[["sysname"]])
-  expect_snapshot(suppressWarnings(measure_directory(corrpath_dir, write_images = T, eye_method = T, scaling_factor = 102)), variant = Sys.info()[["sysname"]])
-})
-
-test_that("add_info works", {
-  add_info(100, corrpath_dir)
-  expect_snapshot_file(paste0(corrpath_dir, "/results/info.txt"), "info.txt")
 })
